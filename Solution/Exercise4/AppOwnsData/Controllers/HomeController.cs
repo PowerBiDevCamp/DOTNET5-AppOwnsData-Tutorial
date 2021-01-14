964f6d101d3ec0e2d8802c5e14a43b0807ddd751
Ted Pattison <ted@tedpattison.net>
2021-01-14 15:07:17 -0500

    Updates

--- Solution/Exercise4/AppOwnsData/Controllers/HomeController.cs
+++ Solution/Exercise4/AppOwnsData/Controllers/HomeController.cs
@@ -30,8 +30,8 @@ namespace AppOwnsData.Controllers
         public async Task<IActionResult> Embed() {
 
             // replace these two GUIDs with the workspace ID and report ID you recorded earlier
-            Guid workspaceId = new Guid("ddab987a-7941-4eb5-a084-370d422dfcac");
-            Guid reportId = new Guid("3e75d100-b21a-4129-8e84-6c6e90ccbd7e");
+            Guid workspaceId = new Guid("11111111-1111-1111-1111-111111111111");
+            Guid reportId = new Guid("22222222-2222-2222-2222-222222222222");
 
             var viewModel = await powerBiServiceApi.GetReport(workspaceId, reportId);
 
